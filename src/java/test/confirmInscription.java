f01e1fd511c428ddbdd8d0deecd67c4ee85cfe39
Victor PINQUIER <vic.pinquier@orange.fr>
2016-04-27 12:29:02 +0200

    Erreurs Inscriptions

--- src/java/test/confirmInscription.java
+++ src/java/test/confirmInscription.java
@@ -1,12 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package test;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -19,76 +21,125 @@ import javax.servlet.http.HttpServletResponse;
  */
 @WebServlet(name = "confirmInscription", urlPatterns = {"/confirmInscription"})
 public class confirmInscription extends HttpServlet {
+    public static final String VUE = "/WEB-INF/choisirCreneauEtu.jsp";
+    public static final String ATT_ERREURS  = "erreurs";
+    public static final String ATT_RESULTAT = "resultat";
 
     // On définit la configuration d'accès au serveur SQL
     
     private static final String URL = "jdbc:mysql://localhost:3306/osezlessciences";
     private static final String USERNAME = "root";
     private static final String PASSWORD = "";
+    Connection connexion = null;
+    Statement statement = null;
+    ResultSet resultat = null;
+    
+    
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )   throws ServletException, IOException {
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+
+    }
+    
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response ) throws ServletException, IOException{
+        String resultat;
+        Map<String, String> erreurs = new HashMap<String, String>();
+        
+        /* Récupération des champs du formulaire. */
+        String prenom = request.getParameter("prenom");
+        String nom = request.getParameter("nom");
+        String mail = request.getParameter("mail");
+        String mdp = request.getParameter("mdp");
+        String confirmation = request.getParameter("confirmation");
+        
+        /* Validation du champ nom. */
+        try {
+            validationNom(nom);
+        } catch (Exception e) {
+            erreurs.put("nom", e.getMessage());
+        }
+        
+        /* Validation du champ prenom. */
+        try {
+            validationPrenom(prenom);
+        } catch (Exception e) {
+            erreurs.put("prenom", e.getMessage());
+        }
+        
+        /* Validation du champ email. */
+        try {
+            validationEmail(mail);
+        } catch (Exception e) {
+            erreurs.put("mail", e.getMessage());
+        }
+        
+        /* Validation des champs mot de passe et confirmation. */
+        try {
+            validationMotsDePasse(mdp, confirmation);
+        } catch (Exception e) {
+            erreurs.put("mdp", e.getMessage());
+        }
+        
+        /* Initialisation du résultat global de la validation. */
+        if (erreurs.isEmpty()) {
+            resultat = "Succès de l'inscription.";
+        } else {
+            resultat = "Échec de l'inscription.";
+        }
+        
+        /* Stockage du résultat et des messages d'erreur dans l'objet request */
+        request.setAttribute(ATT_ERREURS, erreurs);
+        request.setAttribute(ATT_RESULTAT, resultat);
+
+        /* Transmission de la paire d'objets request/response à notre JSP */
+        this.getServletContext().getRequestDispatcher( VUE ).forward( request, response );
+    }
     
     /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * Valide le nom.
      */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet confirmInscription</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet confirmInscription at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+    private void validationNom(String nom) throws Exception {
+        if ( nom == null) {
+            throw new Exception("Merci de saisir un nom.");
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    
     /**
-     * Handles the HTTP <code>GET</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
+     * Valide le prenom.
      */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
+    private void validationPrenom( String prenom ) throws Exception{
+        if ( prenom == null) {
+            throw new Exception("Merci de saisir un prenom.");
+        }
     }
-
+    
     /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
+    * Valide l'adresse mail saisie.
+    */
+    private void validationEmail(String email) throws Exception {
+       if ( email != null && email.trim().length() != 0 ) {
+           if ( !email.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)") ) {
+               throw new Exception("Merci de saisir une adresse mail valide.");
+           }
+       } else {
+           throw new Exception("Merci de saisir une adresse mail.");
+       }
+   }
+   
     /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+    * Valide les mots de passe saisis.
+    */
+    private void validationMotsDePasse(String motDePasse, String confirmation) throws Exception{
+       if (motDePasse != null && motDePasse.trim().length() != 0 && confirmation != null && confirmation.trim().length() != 0) {
+           if (!motDePasse.equals(confirmation)) {
+               throw new Exception("Les mots de passe entrés sont différents, merci de les saisir à nouveau.");
+           } else if (motDePasse.trim().length() < 3) {
+               throw new Exception("Les mots de passe doivent contenir au moins 3 caractères.");
+           }
+       } else {
+           throw new Exception("Merci de saisir et confirmer votre mot de passe.");
+       }
+   }
 
 }
